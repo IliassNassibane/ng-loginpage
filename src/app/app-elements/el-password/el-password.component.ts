db13adeb10faff44eeb436a2da87039ddee69f49
Iliass Nassibane <iliass_nassibane@live.com>
2019-07-26 17:12:06 +0100

    [ADD] experiment test() to acquire parent.id

--- src/app/app-elements/el-password/el-password.component.ts
+++ src/app/app-elements/el-password/el-password.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+//import * as angular from 'jquery';
+declare var angular: any;
 
 @Component({
   selector: 'app-el-password',
@@ -12,4 +14,14 @@ export class ElPasswordComponent implements OnInit {
   ngOnInit() {
   }
 
+  private test() {
+    let _1docActiveElement = document.activeElement;
+    let _2ParentElement = document.activeElement.parentElement;
+    let _3ParentNode = document.activeElement.parentNode;
+    let _4stackOverflow = angular.element(Element).parent().attr('id')
+
+    alert(
+      '1. document.activeElement.id: ' + _1docActiveElement.toString() + '\n2. document.activeElement.parentElement.id: ' + _2ParentElement.toString() + '\n3. document.activeElement.parentNode: ' + _3ParentNode.toString() + '\n4. _4stackOverflow: ' + _4stackOverflow.toString()
+    );
+  }
 }
