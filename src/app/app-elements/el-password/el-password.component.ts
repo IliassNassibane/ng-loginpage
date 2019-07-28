166e42fe16f501d1f2db1603d0b98beb0c9508bd
Iliass Nassibane <iliass_nassibane@live.com>
2019-07-28 23:57:50 +0100

    [UPDATE] Fixed ng serve issue

--- src/app/app-elements/el-password/el-password.component.ts
+++ src/app/app-elements/el-password/el-password.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-//import * as angular from 'jquery';
-declare var angular: any;
 
 @Component({
   selector: 'app-el-password',
@@ -18,10 +16,11 @@ export class ElPasswordComponent implements OnInit {
     let _1docActiveElement = document.activeElement;
     let _2ParentElement = document.activeElement.parentElement;
     let _3ParentNode = document.activeElement.parentNode;
-    let _4stackOverflow = angular.element(Element).parent().attr('id')
 
     alert(
-      '1. document.activeElement.id: ' + _1docActiveElement.toString() + '\n2. document.activeElement.parentElement.id: ' + _2ParentElement.toString() + '\n3. document.activeElement.parentNode: ' + _3ParentNode.toString() + '\n4. _4stackOverflow: ' + _4stackOverflow.toString()
+      '1. document.activeElement.id: ' + _1docActiveElement.toString() + 
+      '\n2. document.activeElement.parentElement.id: ' + _2ParentElement.toString() + 
+      '\n3. document.activeElement.parentNode: ' + _3ParentNode.toString()
     );
   }
 }
