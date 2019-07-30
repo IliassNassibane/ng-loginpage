46b95a46222ebbf86566507156ac628c0e2fe316
Iliass Nassibane <iliass_nassibane@live.com>
2019-07-30 01:08:01 +0100

    [ADD] show password function

--- src/app/app-elements/el-password/el-password.component.ts
+++ src/app/app-elements/el-password/el-password.component.ts
@@ -7,25 +7,26 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 
 export class ElPasswordComponent implements OnInit {
-  
   @Input()
   childId: string;
 
-  constructor() { }
+  constructor() { 
+  }
 
   ngOnInit() {
   }
 
   private test() {
-    let _1docActiveElement = document.activeElement.id;
-    let _2ParentElement = document.activeElement.parentElement.id;
-    let _3ParentNode = document.activeElement.parentNode;
-
     alert(
-      '1. document.activeElement.id: ' + _1docActiveElement.toString() + 
-      '\n2. document.activeElement.parentElement.id: ' + _2ParentElement.toString() + 
-      '\n3. document.activeElement.parentNode: ' + _3ParentNode.toString() +
-      '\n4. Dit is this.childId: ' + this.childId.toString()
+      'Dit is this.childId: ' + this.childId.toString() 
     );
+  }
+
+  private showPass() {
+    document.getElementById(this.childId).setAttribute("type", "text");
+  }
+
+  private hidePass() {
+    document.getElementById(this.childId).setAttribute("type", "password");
   }
 }
